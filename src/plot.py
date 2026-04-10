7239cb7cced22dfee44da25b06090505b5029c27
Joel Villarreal II <joel.villa2329@gmail.com>
2026-04-10 10:23:27 -0600

    plotting both euclidean and cosine distances

--- src/plot.py
+++ src/plot.py
@@ -1,7 +1,7 @@
 """ For generating plots """
 import matplotlib.pyplot as plt
 
-def plot1(mat_dict, ds, epsilon, num_iter, save_fig=False, show_fig=True):
+def plot1(euc_dict, cos_dict, ds, epsilon, num_iter, save_fig=False, show_fig=True):
     """
     For plotting compare_eigenvectors() in Tester
     Plot the data from the given nested dictionary
@@ -10,18 +10,31 @@ def plot1(mat_dict, ds, epsilon, num_iter, save_fig=False, show_fig=True):
     """
 
     i = 0
-    for name, diffs in mat_dict.items():
-        title = f"JL w/ {name} (epsilon = {epsilon}, num_avg={num_iter}"
+    for name, euc_diffs in euc_dict.items():
+        # One row, two columns of subplots
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14,7))
+
+        cos_diffs = cos_dict[name]
+
+        title = f"JL of {name} (epsilon = {epsilon}, num_avg={num_iter})"
         
-        x_label = "reduced dimension (d)"
-        y_label = "Relative difference in top eigenvectors"
-        
-        plt.plot(ds, diffs)
+        x_label = "Reduced Dimension (d)"
+        y_label = "Difference in Top Eigenvectors"
+
+        # Euclidean Plot
+        ax1.plot(ds, euc_diffs)
+        ax1.set_title("Euclidean Distance")
+        ax1.set_xlabel(x_label)
+        ax1.set_ylabel(y_label)
             
-        plt.title(title)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-        plt.legend()
+        # Cosine Plot
+        ax2.plot(ds, cos_diffs)
+        ax2.set_title("Cosine Distance")
+        ax2.set_xlabel(x_label)
+        ax2.set_ylabel(y_label)
+
+        fig.suptitle(title)
+
         plt.tight_layout()
 
         if save_fig:
