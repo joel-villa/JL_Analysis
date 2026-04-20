4bbb50b0c5856050db59b6f92c03e683e387dfe5
Joel Villarreal II <joel.villa2329@gmail.com>
2026-04-20 10:48:19 -0600

    working test (don't use svd library, must use own implementation)

--- src/tests/svd_tests.py
+++ src/tests/svd_tests.py
@@ -6,7 +6,11 @@ All methods in this file return xs, ys, and labels (for easy plotting)
 
 from scipy.sparse.linalg import svds
 from scipy.linalg import norm # 2-norm by default
+
 import numpy as np
+
+from ..util.eig_functs import euclidean_dist
+from ..util.svd_util import topsing
 
 def baseline_svd_convergence(A, v0, v_star, num_iter, seed):
     """
@@ -22,12 +26,18 @@ def baseline_svd_convergence(A, v0, v_star, num_iter, seed):
         v, _, _ = svds(A, 
                        k=1,        # top eigenvector
                        which='LM', # top eigenvector
-                    #    v0=v, #TODO
+                       v0=v, #TODO
                        maxiter=1, 
                        random_state=seed) #TODO should this be more random (ie: add i to seed)
         
-        euc_dist = norm(v - v_star)
+        v = v.flatten() # make v 1D rather than 2D: (x,) rather than (x,1)
+
+        euc_dist = euclidean_dist(v, v_star)
         ys[i] = euc_dist
+        xs[i] = i
+
+    # print(f"xs: {xs}")
+    # print(f"ys: {ys}")
     
     return xs, ys, f"standard svd"
     
